b13ad81ae52f1d6c61c9438f27291f47011fad62
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-18 13:50:16 +0200

    Initial commit

---
+++ src/main/java/mirkoabozzi/U5S7L3/U5S7L3Application.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.U5S7L3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S7L3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S7L3Application.class, args);
+	}
+
+}
---
+++ src/test/java/mirkoabozzi/U5S7L3/U5S7L3ApplicationTests.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.U5S7L3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S7L3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
